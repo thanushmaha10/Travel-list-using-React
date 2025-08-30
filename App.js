734212b025625caf3f7091d639e5654290d5ba5d
Thanush maha <138698852+thanushmaha10@users.noreply.github.com>
2025-08-30 16:07:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,11 +4,6 @@ import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 1, packed: false },
-// ];
 
 export default function App() {
   const [items, setItems] = useState([]);
